2511cf4e3c7adb612d1d8565947f695cc4fe3073
Maksym Martyn <135046406+makson4ik7557@users.noreply.github.com>
2025-10-17 13:32:34 +0300

    Update portfolio_holder.py

--- portfolio_holder.py
+++ portfolio_holder.py
@@ -1,11 +1,14 @@
 class PortfolioHolder:
+    DIVERSIFICATION_HIGH_RISK_THRESHOLD = 5
+    DIVERSIFICATION_MODERATE_RISK_THRESHOLD = 15
+
     def __init__(self, risk_appetite: str = "Moderate"):
         self.risk_appetite = risk_appetite
 
     def calculate_risk_level(self, diversification_score: int) -> str:
-        if diversification_score > 15:
+        if diversification_score > PortfolioHolder.DIVERSIFICATION_MODERATE_RISK_THRESHOLD:
             risk = "Low"
-        elif diversification_score > 5:
+        elif diversification_score > PortfolioHolder.DIVERSIFICATION_HIGH_RISK_THRESHOLD:
             risk = "Moderate"
         else:
             risk = "High"
